acb3bfdb95720761a8625fe1a9885b6613ecbceb
xiapuling <dayuandizangwangpusa@gmail.com>
2022-09-21 05:58:51 +0000

    Bug fix week7

--- src/pinata.js
+++ src/pinata.js
@@ -11,9 +11,8 @@ export const uploadJSONToIPFS = async(JSONBody) => {
     return axios 
         .post(url, JSONBody, {
             headers: {
-                // pinata_api_key: key,
-                // pinata_secret_api_key: secret,
-                authorization: "Bearer "+process.env.REACT_APP_PINATA_JWT
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
             }
         })
         .then(function (response) {
@@ -70,9 +69,8 @@ export const uploadFileToIPFS = async(file) => {
             maxBodyLength: 'Infinity',
             headers: {
                 'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-                // pinata_api_key: key,
-                // pinata_secret_api_key: secret,
-                authorization: "Bearer "+process.env.REACT_APP_PINATA_JWT
+                pinata_api_key: key,
+                pinata_secret_api_key: secret,
             }
         })
         .then(function (response) {
